89ed2cf0e045619e5fb1d5dfed7e2f892b6b45d9
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-15 11:20:24 +0800

    inatra

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,9 +1,23 @@
 module Inatra
   class << self
-    def routes
+    def routes(&block)
+      instance_eval(&block)
+    end
+
+    def method_missing(method, url, &block)
+      method_name = method.to_s.upcase
+      @route ||= {}
+      if @route.key?(method_name)
+        @route[method_name][url] = yield
+      else
+        @route[method_name] = { url => yield }
+      end
     end
 
     def call(env)
+      method_name = env['REQUEST_METHOD']
+      path = env['PATH_INFO']
+      @route[method_name][path]
     end
   end
 end
