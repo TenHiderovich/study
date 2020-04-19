d70bb8cd0196b12ebb1f1c103e73c87750df0d93
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-19 15:04:28 +0800

    rewritten search method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -10,8 +10,26 @@ module Exercise
       end
 
       def search(array, query)
-        result = array.index(query)
-        result.nil? ? -1 : result
+        return -1 if array.empty?
+
+        length = array.length
+
+        if length == 1
+          array[0] == query ? 0 : -1
+        end
+
+        middle = length / 2
+        value = array[middle] - query
+
+        if value == 0
+          middle
+        elsif value > 0
+          search_result = search(array[0..middle - 1], query)
+          search_result == -1 ? -1 : search_result
+        else
+          search_result = search(array[middle + 1..length - 1], query)
+          search_result == -1 ? -1 : search_result + middle + 1
+        end
       end
     end
   end
