32d7dd6100509ce1947191425f76be75eeefbc66
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-04-21 20:39:58 +0800

    rebase master

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,34 +4,35 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        filtered_films = _array
-          .select { |film|
-            countries = film["country"]
-            !countries.nil? && countries.split(',').size >= 2
-          }.select { |film|
-            rating = film["rating_kinopoisk"].to_f
-            !rating.nil? && rating > 0.0
-          }
+      def rating(array)
+        filtered_country = array.select do |film|
+          countries = film['country']
+          countries && countries.split(',').size >= 2
+        end
 
-        ratings = filtered_films.map { |film| film["rating_kinopoisk"].to_f }
-        sum_rating = ratings.reduce(0.0) { |sum, rating| sum += rating }
+        filtered_rating = filtered_country.select do |film|
+          rating = film['rating_kinopoisk'].to_f
+          rating && rating > 0.0
+        end
 
-        sum_rating / filtered_films.size
+        ratings = filtered_rating.map { |film| film['rating_kinopoisk'].to_f }
+        # |a, e| требование rubocop
+        sum_rating = ratings.reduce(0.0) { |a, e| a + e }
+
+        sum_rating / ratings.size
       end
 
-      def chars_count(_films, _threshold)
-        filtered_films = _films
-          .select { |film|
-            rating = film["rating_kinopoisk"].to_f
-            film if (rating >= _threshold)
-          }
-        
-        i_count = filtered_films.reduce(0) { |acc, film|
-          name = film["name"]
-          name.each_char do |char| acc += 1 if char == 'и' end
+      def chars_count(films, threshold)
+        filtered_films = films.select do |film|
+          rating = film['rating_kinopoisk'].to_f
+          film if rating >= threshold
+        end
+
+        i_count = filtered_films.reduce(0) do |acc, film|
+          name = film['name']
+          name.each_char { |char| acc += 1 if char == 'и' }
           acc
-        }
+        end
 
         i_count
       end
