15f8c174c3a7b76aca2ddf1e170b13eede8ddf01
Artem Gapeevtsev <agapeevtsev@yandex.ru>
2020-06-07 20:03:31 +0800

    fix: rubocop

--- test/exercise/rackup/inatra.rb
+++ test/exercise/rackup/inatra.rb
@@ -1,14 +1,24 @@
 module Inatra
   class << self
+    METHODS = %i[get post].freeze
+
     def routes(&block)
+      @route ||= {}
       instance_eval(&block)
     end
 
     def method_missing(method, url)
-      @route ||= {}
-      method_name = method.to_s.upcase
-      @route[method_name] ||= {}
-      @route[method_name][url] = yield
+      if METHODS.include?(method)
+        method_name = method.to_s.upcase
+        @route[method_name] ||= {}
+        @route[method_name][url] = yield
+      else
+        super
+      end
+    end
+
+    def respond_to_missing?(method)
+      METHODS.include?(method)
     end
 
     def call(env)
